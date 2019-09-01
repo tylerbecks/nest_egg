d69c424fb3a08312cac14823e189f1087367a20a
Tyler Becks <tylerdbecks@gmail.com>
2019-09-01 16:39:51 +0200

    Extract binary_search logic

--- nest_egg.py
+++ nest_egg.py
@@ -10,36 +10,35 @@ INFLATION_RATE = 1.022
 LOW_NEST_EGG = 100000
 
 
-def get_inflation_adjustment(num, years):
-    # 2.2% inflation rate is the median of the monthly inflation rates from 2000 - 2019
-    # based on this data set: https://www.usinflationcalculator.com/inflation/historical-inflation-rates/
-    for _ in range(years):
-        num = num * INFLATION_RATE
-
-    return num
-
-
-def calc_nest_egg(annual_withdrawal, low_nest_egg=LOW_NEST_EGG, high_nest_egg=None):
+def calc_nest_egg(annual_withdrawal):
     """Use a binary search to find the needed nest_egg to retire at RETIREMENT_AGE, assuming you will live till DEATH_AGE
-      TODO explain what high and low are
-      High and Low are roughly measured by:
+
+      High and Low are rough guesses to start the binary search. They are calculated as:
         - beginning low estimate: $100,000
         - beginning high estimate: The annual withdrawal amount multiplied by the number of years in retirement.
             With the assumption that the market will grow every year by MARKET_GROWTH, this estimate is safely a high estimate
     """
-    if high_nest_egg is None:
-        high_nest_egg = annual_withdrawal * YEARS_IN_RETIREMENT
+    high_nest_egg = annual_withdrawal * YEARS_IN_RETIREMENT
 
-    mid_nest_egg = (high_nest_egg + low_nest_egg) / 2
-    remainder = get_remainder_after_death(mid_nest_egg, annual_withdrawal)
+    def get_remainder_from_guess(nest_egg_guess):
+        return get_remainder_after_death(nest_egg_guess, annual_withdrawal)
+
+    return binary_search(LOW_NEST_EGG, high_nest_egg, get_remainder_from_guess)
+
+
+def binary_search(low, high, get_remainder):
+    """This is a unique binary search that tries to find a number that falls within the range 0 and 1.
+    """
+    mid = (high + low) / 2
+    remainder = get_remainder(mid)
 
     if remainder >= 0 and remainder < 1:
-        return mid_nest_egg
+        return mid
     elif remainder < 0:
-        return calc_nest_egg(annual_withdrawal, mid_nest_egg, high_nest_egg)
+        return binary_search(mid, high, get_remainder)
     else:
-        return calc_nest_egg(annual_withdrawal, low_nest_egg, mid_nest_egg)
-
+        return binary_search(low, mid, get_remainder)
+    
 
 def get_remainder_after_death(nest_egg, annual_withdrawal):
     for _ in range(YEARS_IN_RETIREMENT):
@@ -51,8 +50,17 @@ def get_remainder_after_death(nest_egg, annual_withdrawal):
     return nest_egg
 
 
+def get_inflation_adjustment(num, years):
+    # 2.2% inflation rate is the median of the monthly inflation rates from 2000 - 2019
+    # based on this data set: https://www.usinflationcalculator.com/inflation/historical-inflation-rates/
+    for _ in range(years):
+        num = num * INFLATION_RATE
+
+    return num
+
 def get_market_growth(amount):
     return amount * MARKET_GROWTH
+
 
 def format_currency(num):
     return "${:,}".format(round(num, 2))
